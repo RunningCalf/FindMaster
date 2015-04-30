4ce7634fa271e68344c8a8dd99e64d8ee7ee1f6d
Xiaokun <xiaokunhou@gmail.com>
2015-04-29 23:07:59 -0400

    some refinement

--- findmaster.py
+++ findmaster.py
@@ -1,11 +1,11 @@
 # all the imports
-import sqlite3
+import sqlite3, os
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
 from contextlib import closing
 
 # configuraion
-#DEBUG = True
+DEBUG = True
 SECRET_KEY = 'find master secrect key'
 
 # create our application
@@ -24,9 +24,18 @@ def show_entries():
             serverinfo = line.split()
             if len(serverinfo) == 3:
                 entries.append(dict(appname=serverinfo[0], port=serverinfo[1], link=serverinfo[2]))
+            elif len(serverinfo) == 2:
+                entries.append(dict(appname=serverinfo[0], port=serverinfo[1], link=""))
 
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/event', methods=['POST'])
+def event_handler():
+    #print request.eventType
+    if request.mimetype == 'application/json' and request.json.get('eventType') == 'api_post_event' :
+        #print request.json        
+        os.system("/data/FindMaster/server_info_update.sh '/data/FindMaster'")        
+    return ""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
