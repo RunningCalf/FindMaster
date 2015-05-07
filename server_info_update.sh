d79fb580a9390aa05b1e76398d530332d6f9d6d8
Xiaokun <xiaokunhou@gmail.com>
2015-05-07 02:24:24 -0400

    add cron file

--- server_info_update.sh
+++ server_info_update.sh
@@ -2,7 +2,7 @@
 workdir=$1
 infofile="server_info.txt"
 
-curl -sSfLk -m 10 -H 'Accept: text/plain' localhost:8080/v2/tasks > /tmp/${infofile}
+curl -sSfLk -m 10 -H 'Accept: text/plain'  http://10.27.17.46:8080/v2/tasks > /tmp/${infofile}
 
 if ! [ -f  ${workdir}/${infofile} ]
 then 
